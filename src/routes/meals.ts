73a5aaec5c58ef9199432112820d23192af878b8
Pedro <pedromiguelboce@gmail.com>
2025-06-09 20:36:44 -0300

    feat: enhance meal retrieval and summary functionality

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -44,6 +44,7 @@ export async function mealsRoutes(app: FastifyInstance) {
       const meals = await knex('meals')
         .select('*')
         .where('user_id', request.user?.id)
+        .orderBy('date')
       return {
         meals,
       }
@@ -54,16 +55,22 @@ export async function mealsRoutes(app: FastifyInstance) {
     {
       preHandler: [checkSessionIdExists],
     },
-    async (request) => {
+    async (request, response) => {
       const getMealParamsSchema = z.object({
         mealId: z.string().uuid(),
       })
 
       const { mealId } = getMealParamsSchema.parse(request.params)
 
-      const meals = await knex('meals').where({ id: mealId }).first()
+      const meal = await knex('meals')
+        .where('user_id', request.user?.id)
+        .where({ id: mealId })
+        .first()
+      if (!meal) {
+        return response.status(404).send({ error: 'Meal ID not found' })
+      }
 
-      return meals
+      return { meal }
     },
   )
   app.put(
@@ -89,7 +96,10 @@ export async function mealsRoutes(app: FastifyInstance) {
         request.body,
       )
 
-      const meal = await knex('meals').where({ id: mealId }).first()
+      const meal = await knex('meals')
+        .where('user_id', request.user?.id)
+        .where({ id: mealId })
+        .first()
 
       if (!meal) {
         return response.status(404).send({ error: 'Meal ID not found' })
@@ -103,7 +113,7 @@ export async function mealsRoutes(app: FastifyInstance) {
           is_on_diet: isOnDiet,
           date: moment(date).format('YYYY-MM-DD HH:mm:ss'),
         })
-      return response.status(201).send()
+      return response.status(204).send()
     },
   )
 
@@ -125,8 +135,57 @@ export async function mealsRoutes(app: FastifyInstance) {
         return response.status(404).send({ error: 'Meal ID not found' })
       }
 
-      await knex('meals').where({ id: mealId }).delete()
+      await knex('meals')
+        .where('user_id', request.user?.id)
+        .where({ id: mealId })
+        .delete()
+
       return response.status(204).send()
+    },
+  )
+
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const summary = await knex('meals')
+        .where('user_id', request.user?.id)
+        .orderBy('date')
+        .select([
+          knex.raw('COUNT(*) as totalMeals'),
+          knex.raw(
+            'COUNT(CASE WHEN is_on_diet = true THEN 1 END) as totalMealsOnDiet',
+          ),
+          knex.raw(
+            'COUNT(CASE WHEN is_on_diet = false THEN 1 END) as totalMealsOffDiet',
+          ),
+        ])
+        .first()
+
+      const meals = await knex('meals')
+        .where('user_id', request.user?.id)
+        .orderBy('date')
+      let maxStreak = 0
+      let currentStreak = 0
+
+      for (const meal of meals) {
+        if (meal.is_on_diet) {
+          currentStreak++
+          if (currentStreak > maxStreak) {
+            maxStreak = currentStreak
+          }
+        } else {
+          currentStreak = 0
+        }
+      }
+      return {
+        summary: {
+          ...summary,
+          bestOnDietStreak: maxStreak,
+        },
+      }
     },
   )
 }
